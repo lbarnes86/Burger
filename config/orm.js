f54bd64833525addd83d7ebc1ce702ff77d84800
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:44:57 -0600

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -4,21 +4,21 @@
 let connection = require("./connection.js");
 
 let orm = {
-    selectAll: function(tableName, cb) {
-        var queryString = `SELECT * FROM ${tableName};`;
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-          cb(result);
-        });
-},
-// vals is an array of values that we want to save to cols
+  selectAll: function (tableName, cb) {
+    var queryString = `SELECT * FROM ${tableName};`;
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // vals is an array of values that we want to save to cols
   // cols are the columns we want to insert the values into
-  insertOne: function(tableName, cols, vals, cb) {
+  insertOne: function (tableName, cols, vals, cb) {
     var queryString = `INSERT INTO ${tableName} (${cols}) VALUES ('${vals}');`;
-     //`INSERT INTO burgers (burger_name devoured) VALUES ('blue cheese',false)`
-    connection.query(queryString, function(err, result) {
+    //`INSERT INTO burgers (burger_name devoured) VALUES ('blue cheese',false)`
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
@@ -28,22 +28,24 @@ let orm = {
 
   // objColVals would be the columns and values that you want to update
   // an example of objColVals would be {name: panther, sleepy: true}
-  updateOne: function(colVal, id, cb) {
+  updateOne: function (colVal, id, cb) {
     var queryString = `UPDATE burgers SET devoured='1' WHERE ${colVal}=${id};`;
-    connection.query(queryString, [id], function(err, result) {
+    connection.query(queryString, [id], function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
-},
-delete: function( tableName, id, cb) {
-        var queryString = `DELETE FROM ${tableName} WHERE id=${id};`;
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-          cb(result);
-        });
-}
-  }
+  },
+  delete: function (tableName, id, cb) {
+    var queryString = `DELETE FROM ${tableName} WHERE id=${id};`;
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
