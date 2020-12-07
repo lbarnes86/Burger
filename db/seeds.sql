061718e9c604e23cb35aa4a0093196adedc61961
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:59:36 -0600

    updated seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
