425b41125c260cda2dcc6ee8d4a62bcccc6d9f3d
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:39:28 -0600

    updated orm.js

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+// Here is the O.R.M. where you write functions that takes inputs and conditions
+// and turns them into database commands like SQL.
+
+let connection = require("./connection.js");
+
+let 
