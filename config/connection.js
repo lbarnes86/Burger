1e623200139e0eb7d86e7d92db9ba3b1b76c134f
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:21:15 -0600

    updating connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
-let mysql = require('mysql');
+let mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: 8080,
-    user: 'root',
-    password: 'rootroot'
-    database: 'burgers_db',
-})
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db",
+});
+// Connection 
