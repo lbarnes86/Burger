3c4d315f7d4498f23a984ca2843cce6becb3ef9a
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:23:00 -0600

    updating connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,23 @@ let connection = mysql.createConnection({
 });
 // Connection 
 connection.connect(function (err) {
-    if (err)  {
-        console.log("connected as id " + )
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+      }
+      console.log("connected as id " + connection.threadId);
+    });
+    
+    //Jaws.db
+    var connection;
+    if (process.env.JAWSDB_URL) {
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+      connection = mysql.createConnection({
+        host: "localhost",
+        port: process.env.PORT || 3306,
+        user: "root",
+        password: "Bob@@1#er",
+        database: "burgers_db",
+      });
     }
-})
