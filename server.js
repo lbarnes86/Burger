59ad6aa701cdf40473e339a0dc187b20a7d0f3b3
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:56:52 -0600

    updated server.js

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("./public"));
+
+// Set Handlebars.
+let exhbs = require("express-handlebars");
+
+app.engine("handlebars", exhbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
