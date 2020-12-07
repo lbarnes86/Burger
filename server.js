da9978eed1a35efa76d0857d43f090a6f026d969
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-06 22:56:19 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 let express = require("express");
 let connection = require("./config/connection");
 let bodyParser = require("body-parser");
+let PORT = process.env.PORT || 8090;
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
